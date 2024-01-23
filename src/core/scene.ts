a275300442fad26c1c56e6c3ce1749d54e7e8859
Nick van Urk <nickyvanurk@gmx.com>
2024-01-23 19:44:54 +0100

    Add gui for testing

--- src/core/scene.ts
+++ src/core/scene.ts
@@ -1,3 +1,4 @@
+import GUI from 'lil-gui';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
@@ -19,14 +20,28 @@ export class Scene {
     this.camera = new THREE.PerspectiveCamera(71, window.innerWidth / window.innerHeight, 0.1, 1000);
     this.camera.position.set(5, 5, 5);
 
+    window.addEventListener('resize', this.handleResize.bind(this), false);
+
+    new OrbitControls(this.camera, this.renderer.domElement);
+
+    // Green cube
     const geometry = new THREE.BoxGeometry(1, 1, 1);
     const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
     this.cube = new THREE.Mesh(geometry, material);
     this.scene.add(this.cube);
 
-    new OrbitControls(this.camera, this.renderer.domElement);
+    // Test GUI
+    const gui = new GUI();
+    const config = {
+      clippingPlaneHeight: 0,
+    };
 
-    window.addEventListener('resize', this.handleResize.bind(this), false);
+    gui
+      .add(config, 'clippingPlaneHeight', -100, 100, 2)
+      .name('Clipping Plane')
+      .onChange((value: number) => {
+        console.log(value);
+      });
   }
 
   render() {
