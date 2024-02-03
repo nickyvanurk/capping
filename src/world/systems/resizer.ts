b69c66235d5f39db7859c834f90cf7e867c752c9
Nick van Urk <nickyvanurk@gmx.com>
2024-02-03 16:52:12 +0100

    Set max dpr of 2

--- src/world/systems/resizer.ts
+++ src/world/systems/resizer.ts
@@ -12,7 +12,7 @@ export class Resizer {
 
 function setSize(container: HTMLDivElement, renderer: WebGLRenderer, camera: PerspectiveCamera) {
   renderer.setSize(container.clientWidth, container.clientHeight);
-  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
   camera.aspect = container.clientWidth / container.clientHeight;
   camera.updateProjectionMatrix();
 }
